8f4b73ea16bd165b698441b689b60c270893a9c9
fahmed <fahmed@lendingclub.com>
2020-03-25 17:06:45 -0700

    first draft

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { FirstPerson, SecondPerson, PersonSwitcher } from "./components";
+import "./index.css";
+
+const App = () => (
+  <BrowserRouter>
+    <>
+      <PersonSwitcher />
+      <Switch>
+        <Route path="/" component={FirstPerson} exact />
+        <Route path="/first-person" component={FirstPerson} exact />
+        <Route path="/second-person" component={SecondPerson} exact />
+      </Switch>
+    </>
+  </BrowserRouter>
+);
+
+export default App;
